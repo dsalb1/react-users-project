78bf72d6f49de444e122ea652e173bd4814d3654
dsalb1 <daniel.j.salberg@gmail.com>
2023-03-21 21:19:20 -0500

    master - better input validation

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -6,17 +6,36 @@ import styles from "./UserForm.module.css";
 const UserForm = (props) => {
     const [enteredUsername, setEnteredUsername] = useState('');
     const [enteredNumber, setEnteredNumber] = useState('');
+    const [isUsernameValid, setIsUsernameValid] = useState(true);
+    const [isNumberValid, setIsNumberValid] = useState(true);
+
 
     const usernameHandler = event => {
+        setIsUsernameValid(true);
         setEnteredUsername(event.target.value);
     }
 
     const numberHandler = event => {
+        setIsNumberValid(true);
         setEnteredNumber(event.target.value);
+    }
+
+    const validateInput = () => {
+        let isValid = true;
+        if (enteredNumber.trim().length === 0) {
+            isValid = false;
+            setIsNumberValid(false);
+        }
+        if (enteredUsername.trim().length === 0) {
+            isValid = false;
+            setIsUsernameValid(false);
+        }
+        return ((isValid) ? true : false);
     }
 
     const onSubmitHandler = event => {
         event.preventDefault();
+        if (!validateInput()) {return;}
         const newUser = {
             username: enteredUsername,
             age: enteredNumber,
@@ -29,10 +48,10 @@ const UserForm = (props) => {
 
     return (
         <form onSubmit={onSubmitHandler} className={styles['form-control']}>
-            <label>Username</label>
-            <input type="text" name="username" value={enteredUsername} onChange={usernameHandler}></input>
-            <label>Age (Years)</label>
-            <input type="number" name="age" value={enteredNumber} onChange={numberHandler}></input>
+            <label className={!isUsernameValid && styles.invalid} >Username</label>
+            <input className={!isUsernameValid && styles.invalid} type="text" name="username" value={enteredUsername} onChange={usernameHandler}></input>
+            <label className={!isNumberValid && styles.invalid} >Age (Years)</label>
+            <input className={!isNumberValid && styles.invalid} type="number" name="age" value={enteredNumber} onChange={numberHandler}></input>
             <Button type="submit">Add User</Button>
         </form>
     )
